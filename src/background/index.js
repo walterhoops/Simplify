c05dfa8586f844289a8ae9494cc7a39c1b48553f
Kenny Cheng <kennyjiahuacheng@gmail.com>
2023-01-21 23:34:59 -0800

    adding logging for highlighted text;

--- src/background/index.js
+++ src/background/index.js
@@ -11,15 +11,18 @@ function initializeContextMenus() {
         chrome.contextMenus.removeAll();
 
         chrome.contextMenus.create({ title: 'Test', id: 'test', contexts: ['selection'] });
+        chrome.contextMenus.create({ title: 'logHighlightedText', id: 'logHighlightedText', contexts: ['selection'] });
     });
 }
 
 function initializeContextMenuEventListeners() {
-    chrome.contextMenus.onClicked.addListener(({ menuItemId }) => {
+    chrome.contextMenus.onClicked.addListener(({ menuItemId, selectionText }) => {
         switch (menuItemId) {
             case 'test':
                 console.log("testing context menu")
                 break;
+            case 'logHighlightedText':
+                console.log(selectionText)
         }
     });
 }
