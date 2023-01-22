6e27beb58c86d3b7322a4da0af98b5ba78b62d26
kennyjhcheng <kennyjiahuacheng@gmail.com>
2023-01-21 23:15:55 -0800

    added initialize export in contenScripts

--- contentScript.js
+++ contentScript.js
@@ -2,5 +2,6 @@
 (async () => {
     const src = chrome.runtime.getURL('src/contentScripts/index.js');
     const contentScript = await import(src);
+    console.log(contentScript);
     contentScript.initialize();
 })();
